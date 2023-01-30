3ba20dcf943ee1df0761bcb17d1b336cba0d6440
Henry <henryonyemaobi@gmail.com>
2023-01-30 16:44:48 +0100

    called api successfully

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,48 +1,14 @@
 <template>
-  <v-app light>
-   <!-- <v-navigation-drawer
-    color="primary"
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>-->
+  <v-app style="background-color: white;">
+  
     <v-app-bar
-    color="primary"
+     color="#4a9ec1"
       :clipped-left="clipped"
       fixed
       app
     >
-    <!--
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-    -->
     <v-icon>mdi-heart</v-icon>
       <v-toolbar-title>{{ title }}</v-toolbar-title>
-      
       
     </v-app-bar>
     
@@ -56,29 +22,28 @@
  
     
     <div>
-  <v-bottom-navigation :elevation="3" fixed>
-<v-btn value="recent"  size="x-small">
+  <v-bottom-navigation :elevation="3" fixed style="background-color: #4a9ec1;">
+<v-btn value="recent"  size="x-small" style="color: white !important;">
   <v-icon>mdi-history</v-icon>
   Career
 </v-btn>
 
-<v-btn value="favorites"  size="x-small">
+<v-btn value="favorites"  size="x-small" style="color: white !important;">
   <v-icon>mdi-heart</v-icon>
   Family
 </v-btn>
 
 <v-btn 
-value="nearby" size="x-small">
+value="nearby" size="x-small" style="color: white !important;">
   <v-icon>mdi-map-marker</v-icon>
   Random
 </v-btn>
 
 <v-btn 
-value="nearby" size="x-small">
-  <v-icon>mdi-map-marker</v-icon>
-  Custom
+value="nearby" size="x-small" style="color: white !important;">
+  <v-icon>mdi-more</v-icon>
+  More
 </v-btn>
-
 </v-bottom-navigation>
 </div>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,154 @@
 <template>
 
   <v-row justify="center" align="center">
+    <v-col>
+
+      <template>
+  <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
+      <v-card
+        class="mx-auto"
+        color="#1F7087"
+        elevation="20"
+        py-4 
+        style="margin: 10px;"
+        d-flex
+        v-for='quote in quotes' v-bind:key='quote.id'
+      >
+        <div class="d-flex justify-between">
+          <v-card-actions class="flex-grow-1 flex-column align-start">
+            <div class="text-h6">
+              You said:
+            </div>
+            <div class="text-body-1" style="color: #F5F5F5; word-wrap: break-word;"> {{quote.verse}}</div>
+            
+       <div class="text-caption font-weight-thin" style="background-color: #555; padding:2px; border-radius: 10px;">{{quote.reference}}</div>
 
-  <template>
-  <v-card
-    class="mx-auto"
-    color="info"
-    elevation="20"
-    logo 
-    py-4 
-    style="margin: 10px;"
-    d-flex
-    v-for="n in 3" :key="n"
-  >
-  
-    <div class="d-flex justify-between">
-      <v-card-title class="flex-grow-1 flex-column align-start">
-        <div class="text-h6">
-          You said:
+          </v-card-actions>
+          <v-img
+            contain
+            height="70px"
+            src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+            style="flex-basis: 80px"
+            class="flex-grow-0"
+          ></v-img>
         </div>
-        <div class="text-body-1 font-weight-thin">Ellie Goulding Ellie Goulding Ellie Goulding Ellie Goulding Ellie Goulding Ellie Goulding</div>
 
-        <div class="text-caption font-weight-thin">(Gen 3:16)</div>
-      </v-card-title>
-
-      <v-img
-        contain
-        height="70px"
-        src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-        style="flex-basis: 80px"
-        class="flex-grow-0"
-      ></v-img>
-    </div>
-
-    <v-divider></v-divider>
-
-    <v-card-actions class="pa-2 text-body-2">
-      Therefore: Ellie Goulding Ellie Goulding Ellie Goulding Ellie Goulding Ellie Goulding Ellie Goulding
-      Ellie Goulding Ellie Goulding Ellie Goulding Ellie Goulding Ellie Goulding Ellie Goulding
-    </v-card-actions>
-  </v-card>
+        <v-divider></v-divider>
+        <v-card-actions class="text-body-2" style="color: #F5F5F5;">
+          {{quote.prayer}}
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row justify="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="d-flex justify-center" style="margin-bottom: 65px;">
+        <v-btn color="#555" @click="getquotes()">Load More</v-btn>
+      </div>
+    </v-col>
+  </v-row>
 </template>
-
-  <div class="d-flex align-center" style="margin-bottom: 55px;">
-    <v-btn color="primary">More (On last pressed)</v-btn>
-  </div>
      
     </v-col>
   </v-row>
 
 </template>
 
+
 <script>
 
-import axios from 'axios';
+//import axios from 'axios';
 
-export default {
-  name: 'IndexPage',
-
-  data () {
+ export default {
+          
+   head(){
     return {
-      
+      title: "Web Development Portfolio",
+      meta:[
+        
+  { hid: 'description6', name: 'description', content: 'web development and digital marketing services.' },
+  { name: 'keywords', content: 'seo, web' },
+        
+      ]
     }
   },
-  methods: {
-   
+
+  data () {
+
+  return {
+  quotes:[],
+  loading: true,
+  err: false,
+  //pagination: [],
   }
+  },
+
+  methods: {
+
+  getquotes(page_url){
+
+    this.scrollTop();
+
+    if(page_url){
+     this.$nuxt.$loading.start()
+    }
+
+  var   page_url = page_url || 'http://localhost:8000/api/quotes';
+  fetch(page_url, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Authorization': '5KuSqmiUM8KDgsdGfr8AFU66VSZRFVVGAAFx8nwynYM0CS91nfRocfOag7A9F8ol'
+  }
+  })
+  .then(res => res.json())
+  .then(res=>{
+    this.quotes = res.data;
+   // console.log(this.quotes)
+   // this.loading = false
+    this.$nuxt.$loading.finish()
+   // this.makePagination(res.meta, res.links);
+  })
+  .catch(error =>{
+    console.log(error)  
+   // this.loading = false    
+    this.$nuxt.$loading.finish()
+    this.err = true
+      })
+},
+
+// getquotes method end
+
+scrollTop(){
+  window.scroll({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+},
+/*
+makePagination(meta, links){
+var pagination = {
+current_page: meta.current_page,
+last_page: meta.last_page,
+next_page_url: links.next,
+prev_page_url: links.prev
 }
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+this.pagination = pagination;
+},
+       */         
+},
+//  method end
+
+mounted(){
+ //get quotes
+ this.getquotes()
+ },
+          
+
+ }
+
 </script>
