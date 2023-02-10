e82f2f8216fbafed7de2b37b91fb641878e8e0a5
Henry <henryonyemaobi@gmail.com>
2023-02-10 11:14:19 +0100

    added menu drawer

--- pages/category.vue
+++ pages/category.vue
@@ -4,12 +4,64 @@
    <v-row justify="center" align="center">
      <v-col>
      
-     <!--app bar-->
-  <v-app-bar color="white" fixed app :elevation="0" :shadow="false" style="color: black;">
-  <v-icon  color="black">mdi-hands-pray</v-icon> 
-  <v-toolbar-title>PrayerPath</v-toolbar-title>
-  </v-app-bar>
-  <!--app bar-->
+    <!--app bar-->
+    <v-app-bar color="white" fixed app :elevation="0" :shadow="false" style="color: black;">
+      <v-icon color="black">mdi-hands-pray</v-icon> 
+      <v-toolbar-title>PrayerPath</v-toolbar-title>
+      <v-spacer></v-spacer>
+    <!--  <v-icon style="margin-right: 10px;" color="green">mdi-signal</v-icon>-->
+      <v-app-bar-nav-icon style="color: black;" @click.stop="drawer = !drawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-app-bar-nav-icon>
+    </v-app-bar>
+    <!--app bar-->
+
+
+    <!-- right navigation drawer -->
+    <v-navigation-drawer fixed app v-model="drawer">
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://cdn.pixabay.com/photo/2016/04/15/18/05/computer-1331579_960_720.png"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Welcome</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          link
+          :to="'/favourites'"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Favourites</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-list dense>
+        <v-list-item
+          link
+          :to="'/feature-request'"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-feature-search</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Feature Request</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
   
   
   <template>
@@ -431,6 +483,7 @@ export default {
      categories: [],
      fromLoadMore:null,
      showLoadButton:null,
+     drawer: false,
     };
   },
 
@@ -582,6 +635,15 @@ console.error(error);
     this.getCategories()
 
   window.addEventListener("scroll", this.handleScroll);
+
+  /*
+  var online = navigator.onLine; 
+  if(!online){
+  console.log('Not online')
+   }else{
+   console.log('online')
+   }
+*/
   },
 
   beforeDestroy() {
