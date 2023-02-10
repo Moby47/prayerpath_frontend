4436fa9f895c671fa6a643e35ed469342d398d8e
Henry <henryonyemaobi@gmail.com>
2023-02-10 19:02:19 +0100

    configured prayer page to use components

--- pages/prayer.vue
+++ pages/prayer.vue
@@ -5,188 +5,16 @@
    <v-col>
    
 <!--app bar-->
-<v-app-bar color="white" fixed app :elevation="0" :shadow="false" style="color: black;">
-      <v-icon color="black">mdi-hands-pray</v-icon> 
-      <v-toolbar-title>PrayerPath</v-toolbar-title>
-      <v-spacer></v-spacer>
-    <!--  <v-icon style="margin-right: 10px;" color="green">mdi-signal</v-icon>-->
-      <v-app-bar-nav-icon style="color: black;" @click.stop="drawer = !drawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-app-bar-nav-icon>
-    </v-app-bar>
-    <!--app bar-->
+<AppBar/>
 
 
-    <!-- right navigation drawer -->
-    <v-navigation-drawer fixed app v-model="drawer">
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://cdn.pixabay.com/photo/2016/04/15/18/05/computer-1331579_960_720.png"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>Welcome</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item
-          link
-          :to="'/favourites'"
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-heart</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>Favourites</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-      <v-list dense>
-        <v-list-item
-          link
-          :to="'/feature-request'"
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-feature-search</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>Feature Request</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-    </v-navigation-drawer>
-
-
-<template>
-<v-row justify="center" align="center">
-  <v-col cols="12" sm="8" md="6" class="others-font">
-    <v-card
-      class="mx-auto animated tdFadeIn"
-      color="#ADD8E6"
-      :elevation="0" :shadow="false"
-      py-4
-      style="margin: 10px;"
-      v-for="quote in quotes"
-      v-bind:key="quote.id"
-    >
-      <div class="d-flex justify-between">
-        
-        <v-card-actions class="flex-grow-1 flex-column align-start">
-  <div class="text-h6" style="color: #000;">
-    God's Word:
-  </div>
-  <div class="text-body-1 verse-font" style="color: #000; word-wrap: break-word;">
-    {{quote.verse}}
-  </div>
-  
-    <v-chip
-      @click.prevent="promptRedirect(quote.verse_url,'NIV')"
-      color="#F5F5DC"
-      label
-      size="x-small"
-      text-color="black"
-    >
-    {{quote.reference}}<b>&nbsp NIV</b>
-    </v-chip>
-
-    <div style="display: flex; flex-direction: row;" class="mt-1">
-    
-      <v-chip
-        @click.prevent="promptRedirect(quote.verse_url,'KJV')"
-        color="#F5F5DC"
-        label
-        size="x-small"
-        text-color="black"
-        class="mr-1"
-      >
-        
-        KJV
-      </v-chip>
-      <v-chip
-        @click.prevent="promptRedirect(quote.verse_url,'NKJV')"
-        color="#F5F5DC"
-        label
-        size="x-small"
-        text-color="black"
-        class="mr-1"
-      >
-        
-        NKJV
-      </v-chip>
-
-      <v-chip
-        @click.prevent="promptRedirect(quote.verse_url,'NLT')"
-        color="#F5F5DC"
-        label
-        size="x-small"
-        text-color="black"
-        class="mr-1"
-      >
-        
-        NLT
-      </v-chip>
-    
-
-  </div>
- 
-</v-card-actions>
-
-        <v-img
-          v-if="quote.imageurl"
-          contain
-          height="50px"
-          :lazy-src="quote.imageurl"
-          :src="quote.imageurl"
-          style="flex-basis: 80px"
-          class="flex-grow-0 mr-1 mt-2"
-        ></v-img>
-
-        <v-img
-          v-else
-          contain
-          height="50px"
-          lazy-src="https://cdn.pixabay.com/photo/2014/04/02/10/44/hands-304398_960_720.png"
-          src="https://cdn.pixabay.com/photo/2014/04/02/10/44/hands-304398_960_720.png"
-          style="flex-basis: 80px"
-          class="flex-grow-0 mr-1 mt-2"
-        ></v-img>
-      </div>
-
-      <v-divider></v-divider>
-
-      <v-card-actions class="text-body-2 prayer-font" style="color: #000;">
-        {{quote.prayer}}
-      </v-card-actions>
-      <v-chip
-        class="ma-2"
-        label
-        color="#9AC0D1"
-        style=" font-size: 12px; padding: 3px;"
-        text-color="black"
-      >
-        Prayer For {{quote.category}}
-      </v-chip>
-    </v-card>
-  </v-col>
-</v-row>
-<v-row justify="center">
-  <v-col cols="12" sm="8" md="6">
-    <div class="
-d-flex justify-center" style="margin-bottom: 65px;">
-       <!-- Show reset button if showButton is true -->
-       <v-btn color="#F5F5DC" style="color:#000;" @click="loadMore()" v-if="showLoadButton" depressed>More</v-btn>
-     </div>
-   </v-col>
- </v-row>
- </template>
-<!-- Dynamic content and btn-->
+<QuoteCard
+      :quotes="quotes"
+      :showLoadButton="showLoadButton"
+      @load-more="loadMore"
+      @prompt-redirect="promptRedirect"
+    />
+  <!-- Dynamic content and btn-->
 
 
  <!-- bottom nav -->
@@ -243,7 +71,7 @@ d-flex justify-center" style="margin-bottom: 65px;">
    value="nearby" 
    size="x-small" 
    style="color: black !important;" 
-   @click="dialog = true">
+   @click="openCategoryModal()">
    <v-icon>mdi-dots-vertical</v-icon>
    Others
  </v-btn>
@@ -252,95 +80,20 @@ d-flex justify-center" style="margin-bottom: 65px;">
 <!-- bottom nav -->
 
 
- <!-- Category Modal -->
- <template>
-   <div class="text-center">
-     <v-dialog
-       v-model="dialog">
-       <v-card style="background-color: white !important;">
+  <!-- Category Modal -->
+  <CategoryModal ref="categoryModal" @category-selected="gotocat"/>
+  <!-- Category Modal-->
+  
+  
+   <!--Loading Overlay-->
+   <LoadingOverlay :overlay="overlay"/>
+         <!--Loading Overlay-->
+  
+   
+         <!--Message snackbar-->
+         <MessageSnackBar :timeout="4500" :snackText="snackText"  ref="MessageSnackBar" />
+     <!--Message snackbar-->
 
-         <!-- Categories list --> <!-- For online use/display -->
-         <template v-if="!offlineCategory">
-           <div class="text-center" v-if="categories">
-             <!-- Loop through categories and display each as a chip -->
-             <v-chip
-               class="ma-2"
-               color="#ADD8E6"
-               text-color="black"
-               v-for='category in categories' 
-               v-bind:key='category.id'
-               @click="gotocat(category.category)"
-             >
-               {{category.category}}
-             </v-chip>
-           </div>
-           <div class="text-center" style="color: black !important;" v-else>
-            Nothing to see here.
-           </div>
-         </template>
-
-         <!-- For offline use -->
-         <template v-else>
-           <div class="text-center" v-if="categories">
-             <v-chip
-               class="ma-2"
-               color="#9AC0D1"
-               text-color="black"
-               v-for="category in categories" :key="category"
-               @click="gotocat(category)"
-             >
-               {{category}}
-             </v-chip>
-           </div>
-           <div class="text-center" style="color: black !important;" v-else>
-            Nothing to see here.
-           </div>
-         </template>
-
-       <!-- Close button -->
-       <v-card-actions style="background-color: white !important; display: flex; justify-content: center; align-items: center;">
- <v-btn depressed  color="#F5F5DC" @click="dialog = false" style="color:black">Close</v-btn>
-</v-card-actions>
-
-     </v-card>
-   </v-dialog>
- </div>
-</template>
-<!-- Category Modal-->
-
-
- <!--Loading Overlay-->
-   <template>
-       <div class="text-center">
-         <v-overlay :value="overlay">
-           <v-progress-linear indeterminate size="64" color="#F5F5DC"  height="10"></v-progress-linear>
-           <br>
-           Loading...
-         </v-overlay>
-       </div>
-       </template>
-       <!--Loading Overlay-->
-
- 
-       <!--Err snackbar-->
-   <v-snackbar
-   :timeout="4500"
-   shaped
-   color="#555"
-     v-model="snackbar" >
-     {{errSnackText}}
-     <template v-slot:action="{ attrs }">
-       <v-btn
-       color="#F5F5DC"
-         text
-         v-bind="attrs"
-         @click="snackbar = false"
-       >
-         Ok
-       </v-btn>
-       </template>
-   </v-snackbar>
-   <!--Err snackbar-->
 
 <!-- Verse url snackbar-->
 <v-snackbar
@@ -453,7 +206,7 @@ export default {
  data() {
    return {
      offlineCategory: false,
-     errSnackText:'',
+     snackText:'',
      verse_url:'',
      showSnackbar: false,
      snackbar: false,
@@ -475,6 +228,10 @@ export default {
  },
 
  methods: {
+
+  openCategoryModal () {
+    this.$refs.categoryModal.dialog = true;
+  },
 
 async getquotes() {
 
@@ -507,7 +264,7 @@ this.showLoadButton=true
 this.overlay = false;
 
 if (res.data.data.length === 0) {
-       this.errSnackText = "Alas, our search has come up empty - let's ask the Lord for a miracle!"
+       this.snackText = "Alas, our search has come up empty - let's ask the Lord for a miracle!"
        this.snackbar = true
       }
 
@@ -547,11 +304,11 @@ if (savedQuotes) {
 this.quotes = savedQuotes;
 //console.log('retrieved',savedQuotes)
 this.snackbar = true
-this.errSnackText = "Off the grid? Don't fret, God's got us - Offline mode."
+this.snackText = "Off the grid? Don't fret, God's got us - Offline mode."
 this.showLoadButton=false
 } else {
 console.log("No cached record/quotes found");
-this.errSnackText = "Blackout? Trust in God - No saved records found."
+this.snackText = "Blackout? Trust in God - No saved records found."
 this.snackbar = true
 }
 
