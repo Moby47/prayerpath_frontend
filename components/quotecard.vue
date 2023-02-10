2b3313e9252e68bc69e202f0bd67123c2d64acce
Henry <henryonyemaobi@gmail.com>
2023-02-10 12:48:50 +0100

    quotecard changed to component

---
+++ components/quotecard.vue
@@ -0,0 +1,150 @@
+<template>
+    <div>
+        <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6" class="others-font">
+      <v-card
+        class="mx-auto animated tdFadeIn"
+        color="#ADD8E6"
+        :elevation="0" :shadow="false"
+        py-4
+        style="margin: 10px;"
+        v-for="quote in quotes"
+        v-bind:key="quote.id"
+      >
+        <div class="d-flex justify-between">
+          
+          <v-card-actions class="flex-grow-1 flex-column align-start">
+    <div class="text-h6" style="color: #000;">
+      God's Word:
+    </div>
+    <div class=" verse-font" style="color: #000; word-wrap: break-word;">
+      {{quote.verse}}
+    </div>
+    
+      <v-chip
+        @click.prevent="promptRedirect(quote.verse_url,'NIV')"
+        color="#F5F5DC"
+        label
+        size="x-small"
+        text-color="black"
+      >
+       
+        {{quote.reference}}<b>&nbsp NIV</b>
+      </v-chip>
+  
+      <div style="display: flex; flex-direction: row;" class="mt-1">
+      
+        <v-chip
+          @click.prevent="promptRedirect(quote.verse_url,'KJV')"
+          color="#F5F5DC"
+          label
+          size="x-small"
+          text-color="black"
+          class="mr-1"
+        >
+          
+          KJV
+        </v-chip>
+        <v-chip
+          @click.prevent="promptRedirect(quote.verse_url,'NKJV')"
+          color="#F5F5DC"
+          label
+          size="x-small"
+          text-color="black"
+          class="mr-1"
+        >
+          
+          NKJV
+        </v-chip>
+  
+        <v-chip
+          @click.prevent="promptRedirect(quote.verse_url,'NLT')"
+          color="#F5F5DC"
+          label
+          size="x-small"
+          text-color="black"
+          class="mr-1"
+        >
+          
+          NLT
+        </v-chip>
+      
+  
+    </div>
+   
+  </v-card-actions>
+  
+          <v-img
+            v-if="quote.imageurl"
+            contain
+            height="50px"
+            :lazy-src="quote.imageurl"
+            :src="quote.imageurl"
+            style="flex-basis: 80px"
+            class="flex-grow-0 mr-1 mt-2"
+          ></v-img>
+  
+          <v-img
+            v-else
+            contain
+            height="50px"
+            lazy-src="https://cdn.pixabay.com/photo/2014/04/02/10/44/hands-304398_960_720.png"
+            src="https://cdn.pixabay.com/photo/2014/04/02/10/44/hands-304398_960_720.png"
+            style="flex-basis: 80px"
+            class="flex-grow-0 mr-1 mt-2"
+          ></v-img>
+        </div>
+  
+        <v-divider></v-divider>
+  
+        <v-card-actions class="text-body-2 prayer-font" style="color: #000;">
+          {{quote.prayer}}
+        </v-card-actions>
+        <v-chip
+          class="ma-2"
+          label
+          color="#9AC0D1"
+          style=" font-size: 12px; padding: 3px;"
+          text-color="black"
+        >
+          Prayer For {{quote.category}}
+        </v-chip>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row justify="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="
+  d-flex justify-center" style="margin-bottom: 65px;">
+         <!-- Show reset button if showButton is true -->
+         <v-btn color="#F5F5DC" style="color:#000;" @click="loadMore()" v-if="showLoadButton" depressed>More</v-btn>
+       </div>
+     </v-col>
+   </v-row>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "QuoteCard",
+    props: {
+      quotes: {
+        type: Array,
+        required: true,
+      },
+      showLoadButton: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    methods: {
+      loadMore() {
+        this.$emit("load-more");
+      },
+      promptRedirect(url, version) {
+        this.$emit("prompt-redirect", url, version);
+      },
+    },
+  };
+  </script>
+  
--- pages/category.vue
+++ pages/category.vue
@@ -4,132 +4,15 @@
    <v-row justify="center" align="center">
      <v-col>
      
-   <appbar></appbar>
+   <appbar/>
   
   
-  <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6" class="others-font">
-      <v-card
-        class="mx-auto animated tdFadeIn"
-        color="#ADD8E6"
-        :elevation="0" :shadow="false"
-        py-4
-        style="margin: 10px;"
-        v-for="quote in quotes"
-        v-bind:key="quote.id"
-      >
-        <div class="d-flex justify-between">
-          
-          <v-card-actions class="flex-grow-1 flex-column align-start">
-    <div class="text-h6" style="color: #000;">
-      God's Word:
-    </div>
-    <div class=" verse-font" style="color: #000; word-wrap: break-word;">
-      {{quote.verse}}
-    </div>
-    
-      <v-chip
-        @click.prevent="promptRedirect(quote.verse_url,'NIV')"
-        color="#F5F5DC"
-        label
-        size="x-small"
-        text-color="black"
-      >
-       
-        {{quote.reference}}<b>&nbsp NIV</b>
-      </v-chip>
-  
-      <div style="display: flex; flex-direction: row;" class="mt-1">
-      
-        <v-chip
-          @click.prevent="promptRedirect(quote.verse_url,'KJV')"
-          color="#F5F5DC"
-          label
-          size="x-small"
-          text-color="black"
-          class="mr-1"
-        >
-          
-          KJV
-        </v-chip>
-        <v-chip
-          @click.prevent="promptRedirect(quote.verse_url,'NKJV')"
-          color="#F5F5DC"
-          label
-          size="x-small"
-          text-color="black"
-          class="mr-1"
-        >
-          
-          NKJV
-        </v-chip>
-  
-        <v-chip
-          @click.prevent="promptRedirect(quote.verse_url,'NLT')"
-          color="#F5F5DC"
-          label
-          size="x-small"
-          text-color="black"
-          class="mr-1"
-        >
-          
-          NLT
-        </v-chip>
-      
-  
-    </div>
-   
-  </v-card-actions>
-  
-          <v-img
-            v-if="quote.imageurl"
-            contain
-            height="50px"
-            :lazy-src="quote.imageurl"
-            :src="quote.imageurl"
-            style="flex-basis: 80px"
-            class="flex-grow-0 mr-1 mt-2"
-          ></v-img>
-  
-          <v-img
-            v-else
-            contain
-            height="50px"
-            lazy-src="https://cdn.pixabay.com/photo/2014/04/02/10/44/hands-304398_960_720.png"
-            src="https://cdn.pixabay.com/photo/2014/04/02/10/44/hands-304398_960_720.png"
-            style="flex-basis: 80px"
-            class="flex-grow-0 mr-1 mt-2"
-          ></v-img>
-        </div>
-  
-        <v-divider></v-divider>
-  
-        <v-card-actions class="text-body-2 prayer-font" style="color: #000;">
-          {{quote.prayer}}
-        </v-card-actions>
-        <v-chip
-          class="ma-2"
-          label
-          color="#9AC0D1"
-          style=" font-size: 12px; padding: 3px;"
-          text-color="black"
-        >
-          Prayer For {{quote.category}}
-        </v-chip>
-      </v-card>
-    </v-col>
-  </v-row>
-  <v-row justify="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="
-  d-flex justify-center" style="margin-bottom: 65px;">
-         <!-- Show reset button if showButton is true -->
-         <v-btn color="#F5F5DC" style="color:#000;" @click="loadMore()" v-if="showLoadButton" depressed>More</v-btn>
-       </div>
-     </v-col>
-   </v-row>
-   </template>
+   <quotecard
+      :quotes="quotes"
+      :showLoadButton="showLoadButton"
+      @load-more="loadMore"
+      @prompt-redirect="promptRedirect"
+    />
   <!-- Dynamic content and btn-->
   
   
