a546b18e539031318ae3cd57f3c228fba1277de4
Henry <henryonyemaobi@gmail.com>
2023-03-09 13:42:17 +0100

    test custom push btn

---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <a href="#" id="my-notification-button" style="display: none;">Subscribe to Notifications C</a>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      
+    },
+  };
+  </script>
+  
